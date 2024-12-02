cead92ae3d0674fed1484ad0dbdd5099e622a030
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-12-02 22:06:29 +0100

    part 2 continue

--- Day2/part1.py
+++ Day2/part1.py
@@ -42,24 +42,27 @@ def main(input):
         last_increase = None
         safe_counter = 0
         for i in range(0, len(levels) - 1):
+            first_con = False
+            sec_con = False
             levels[i] = levels[i].replace('\n','')
             diff = int(levels[i]) - int(levels[i + 1])
             if  0 < abs(diff) < 4:
-                if diff > 0:
-                    increase = True
-                else:
-                    increase = False
-                if increase == last_increase or last_increase == None:
-                    safe_counter += 1
-                    last_increase = increase
-                else:
-                    print("unsafe level")
-                    #break #second condition not met
+                first_con = True
             else:
-                print("unsafe level")
-                #break #first condition not met
-            if safe_counter == len(levels) - 1 or safe_counter == len(levels) - 2:
-                reports += 1
+                print(f"unsafe level: diff = {abs(diff)}")
+            if diff > 0:
+                increase = True
+            else:
+                increase = False
+            if increase == last_increase or last_increase == None:
+                sec_con = True
+            else:
+                print(f"unsafe level: {increase} != {last_increase}")
+            if first_con and sec_con:
+                safe_counter += 1
+            last_increase = increase
+        if safe_counter == len(levels) - 1 or safe_counter == len(levels) - 2:
+            reports += 1
         print(f"safe counter: {safe_counter}")
     print(f"Part 2: {reports}")
     #print(f"Part 2: {sim_score}")
