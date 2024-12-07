0e562adcd86a1677435e323df89eb56ab13496dc
Richard Stuart <richard.niklas.stuart@gmail.com>
2024-12-07 18:12:40 +0100

    Day6 part 1

--- Day6/part1.py
+++ Day6/part1.py
@@ -17,33 +17,98 @@ def main(input):
                 coords[f"{x},{y}"] = lines[y][x]
                 temp = coords[f"{x},{y}"]
         guard_start_pos = ""
-        guard_positions = 0
-        guard_direction = "up"
+        guard_direction = ""
         guard_position = ""
+        distinct_positions = 0
 
         for y in range(0,Y_MAX):
             for x in range(0,X_MAX):
                 if coords[f"{x},{y}"] == "^":
                     guard_start_pos = f"{x},{y}"
+                    guard_direction = "^"
+                if coords[f"{x},{y}"] == ">":
+                    guard_start_pos = f"{x},{y}"
+                    guard_direction = ">"
+                if coords[f"{x},{y}"] == "v":
+                    guard_start_pos = f"{x},{y}"
+                    guard_direction = "v"
+                if coords[f"{x},{y}"] == "<":
+                    guard_start_pos = f"{x},{y}"
+                    guard_direction = "<"
         print(f"start pos: {guard_start_pos}")
         guard_position = guard_start_pos
 
+        # i = 0
+        # for y in range(0,Y_MAX):
+        #     i = 0
+        #     for x in range(0,X_MAX):
+        #         i += 1
+        #         #print(i)
+        #         if i == X_MAX:
+        #             print(coords[f"{x},{y}"],end="\n")
+        #             #print("here")
+        #         else:
+        #             print(coords[f"{x},{y}"],end="")
+
+        #Check if #, then update direction
+        #Update position
         try:
             while True:
-                if guard_direction == "up":
-                    if coords[f"{x},{y + 1}" == "#"]:
-                        guard_direction = "right"
-                        coords[guard_position] == "X"
+                cur_x = int(guard_position.split(",")[0])
+                cur_y = int(guard_position.split(",")[1])
+                if guard_direction == "^":
+                    if coords[f"{cur_x},{cur_y - 1}"] == "#":
+                        guard_direction = ">"
+                        coords[guard_position] = guard_direction
                     else:
-                        coords[guard_position] == "X"
-                if guard_direction == "right":
-                    if coords[f"{x + 1},{y}" == "#"]:
-                        guard_direction = "down"
-                        coords[guard_position] == "X"
+                        coords[guard_position] = "X"
+                        guard_position = f"{cur_x},{cur_y - 1}"
+                        coords[guard_position] = "^"
+                elif guard_direction == ">":
+                    if coords[f"{cur_x + 1},{cur_y}"] == "#":
+                        guard_direction = "v"
+                        coords[guard_position] = guard_direction
                     else:
-                        coords[guard_position] == "X"
+                        coords[guard_position] = "X"
+                        guard_position = f"{cur_x + 1},{cur_y}"
+                        coords[guard_position] = ">"
+                elif guard_direction == "v":
+                    if coords[f"{cur_x},{cur_y + 1}"] == "#":
+                        guard_direction = "<"
+                        coords[guard_position] = guard_direction
+                    else:
+                        coords[guard_position] = "X"
+                        guard_position = f"{cur_x},{cur_y + 1}"
+                        coords[guard_position] = "<"
+                elif guard_direction == "<":
+                    if coords[f"{cur_x - 1},{cur_y}"] == "#":
+                        guard_direction = "^"
+                        coords[guard_position] = guard_direction
+                    else:
+                        coords[guard_position] = "X"
+                        guard_position = f"{cur_x - 1},{cur_y}"
+                        coords[guard_position] = "<"
+                #print("---------------")
+                # i = 0
+                # for y in range(0,Y_MAX):
+                #     i = 0
+                #     for x in range(0,X_MAX):
+                #         i += 1
+                #         #print(i)
+                #         if i == X_MAX:
+                #             print(coords[f"{x},{y}"],end="\n")
+                #             #print("here")
+                #         else:
+                #             print(coords[f"{x},{y}"],end="")
         except KeyError:
             print("guard is leaving")
+        
+        distinct_positions += 1 #start pos
+        for y in range(0,Y_MAX):
+            for x in range(0,X_MAX):
+                if coords[f"{x},{y}"] == "X":
+                    distinct_positions += 1
+        print(f"Result part 1: {distinct_positions}")
 
 if __name__ == "__main__":
     main(sys.argv[1])
